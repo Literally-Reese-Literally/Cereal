6afc08e06bf68bdbbf0587ed51e03293f712c809
Lexi <reeseivey21@hotmail.com>
2026-04-25 14:17:48 -0400

    I legit forgot to update version
    nah

--- Src/PluginInfo.cs
+++ Src/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "Lexi.Cereal.Menu";
         public const string Name = "Cereal.Menu";
-        public const string Version = "1.0.7";
+        public const string Version = "1.0.8";
     }
 }
